b69697b52709a43fd63e0d008b30564818943028
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-12-09 17:21:09 -0500

    Uploaded boilerplate code from Spring initializer.

---
+++ src/main/java/com/revature/TicketReimbursement/TicketReimbursementApplication.java
@@ -0,0 +1,13 @@
+package com.revature.TicketReimbursement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketReimbursementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketReimbursementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/TicketReimbursement/TicketReimbursementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.TicketReimbursement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketReimbursementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
