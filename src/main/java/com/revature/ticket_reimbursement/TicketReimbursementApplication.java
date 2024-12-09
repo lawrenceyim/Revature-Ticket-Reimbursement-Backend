d6fbe4c8455b4d06a1b8e3dd7cd49286520521d7
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-12-09 17:26:27 -0500

    Replaced incorrectly uploaded Gradle project with Maven project.

--- src/main/java/com/revature/ticket_reimbursement/TicketReimbursementApplication.java
+++ src/main/java/com/revature/ticket_reimbursement/TicketReimbursementApplication.java
@@ -1,4 +1,4 @@
-package com.revature.TicketReimbursement;
+package com.revature.ticket_reimbursement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/revature/ticket_reimbursement/TicketReimbursementApplicationTests.java
+++ src/test/java/com/revature/ticket_reimbursement/TicketReimbursementApplicationTests.java
@@ -1,4 +1,4 @@
-package com.revature.TicketReimbursement;
+package com.revature.ticket_reimbursement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
