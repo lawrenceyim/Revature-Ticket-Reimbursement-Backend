103c15e8c61b8119f5267bf32e8778f69a40e01b
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-12-10 20:52:38 -0500

    Removed wildcard imports and replaced them with single class imports.

--- src/main/java/com/revature/ticket_reimbursement/entity/Ticket.java
+++ src/main/java/com/revature/ticket_reimbursement/entity/Ticket.java
@@ -1,10 +1,16 @@
 package com.revature.ticket_reimbursement.entity;
 
 import com.revature.ticket_reimbursement.TicketStatus;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
--- src/test/java/com/revature/ticket_reimbursement/GetTicketTests.java
+++ src/test/java/com/revature/ticket_reimbursement/GetTicketTests.java
@@ -16,8 +16,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
