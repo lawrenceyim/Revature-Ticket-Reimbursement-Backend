29a88630f834834b7f2860685baf60bcf50e2244
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-12-12 15:03:44 -0500

    Added CORS for React frontend app's localhost address to controller classes.

--- src/main/java/com/revature/ticket_reimbursement/controller/TicketController.java
+++ src/main/java/com/revature/ticket_reimbursement/controller/TicketController.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/tickets")
+@CrossOrigin(origins = "http://localhost:5173/")
 public class TicketController {
     @Autowired
     private EmployeeService employeeService;
